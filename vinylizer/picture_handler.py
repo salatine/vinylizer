66c0f96758c9a37d3ab4446464bf2fb01480623d
salatine <sawurora@gmail.com>
2024-05-23 21:54:43 -0300

    fucking google api cause every other api sucks

--- vinylizer/picture_handler.py
+++ vinylizer/picture_handler.py
@@ -1,7 +1,14 @@
 import io
 import requests
 from PIL import Image, ImageOps
-from typing import List
+from datetime import datetime
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+import json
+
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth()
+drive = GoogleDrive(gauth)
 
 def compress_picture(picture: str) -> io.BytesIO:
     remove_exif(picture)
@@ -11,17 +18,25 @@ def compress_picture(picture: str) -> io.BytesIO:
     return compressed_picture
 
 def upload_picture(picture: str) -> str:
-    compressed_picture = compress_picture(picture)
-    response = requests.post(
-        'https://tmpfiles.org/api/v1/upload', 
-        files={'file': ('photo.webp', compressed_picture, 'image/webp')}, 
-        data={'reqtype': 'fileupload'},
-    )
-    response.raise_for_status()
-    url = response.json().get('data').get('url')
-    download_url = url.replace('tmpfiles.org', 'tmpfiles.org/dl')
-    return download_url
-    
+    compress_picture(picture)
+    formatDate = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    file = drive.CreateFile(metadata={"title": "{0}.webp".format(formatDate), 'mimeType': 'image/webp'})
+    file.SetContentFile(picture)  # Set content of the file from given string.
+    file.Upload(param={'supportsTeamDrives': True})
+
+    access_token = gauth.credentials.access_token # gauth is from drive = GoogleDrive(gauth) Please modify this for your actual script.
+    file_id = file['id']
+    url = 'https://www.googleapis.com/drive/v3/files/' + file_id + '/permissions?supportsAllDrives=true'
+    headers = {'Authorization': 'Bearer ' + access_token, 'Content-Type': 'application/json'}
+    payload = {'type': 'anyone', 'value': 'anyone', 'role': 'reader'}
+    res = requests.post(url, data=json.dumps(payload), headers=headers)
+    link = file['alternateLink']
+
+    # change to a downloadable link
+    link = link.replace("drive.google.com", "drive.usercontent.google.com").replace("file/d/", "download?id=")
+    link = link[:link.rfind('/')]
+
+    return link
 
 def remove_exif(picture: str) -> None:
     image = Image.open(picture)
